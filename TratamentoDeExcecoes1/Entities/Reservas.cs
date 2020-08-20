fafabea5b556461310eeb4e31e094f998022cfc7
Angelo Carvalho <angelosuporteti@gmail.com>
2020-08-20 12:01:07 -0300

    Tratamento de exceções - exemplo de solução mais complicada usando if e else

---
+++ TratamentoDeExcecoes1/Entities/Reservas.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TratamentoDeExcecoes1.Entities
+{
+    class Reservas
+    {
+        public int RoomNumber { get; set; }
+        public DateTime CheckIn { get; set; }
+        public DateTime CheckOut { get; set; }
+
+        public Reservas()
+        {
+        }
+
+        public Reservas(int roomNumber, DateTime checkIn, DateTime checkOut)
+        {
+            RoomNumber = roomNumber;
+            CheckIn = checkIn;
+            CheckOut = checkOut;
+        }
+
+        public int Duration()
+        {
+            TimeSpan duration = CheckOut.Subtract(CheckIn);//vai fazer a diferença entre a entrada e saída
+            return (int)duration.TotalDays; //Vai retornar a diferença em dias
+        }
+
+        public void UpdadeDates(DateTime checkIn, DateTime checkOut)
+        {
+            CheckIn = checkIn;
+            CheckOut = checkOut;
+        }
+
+
+        public override string ToString()
+        {
+            return "Room "
+                + RoomNumber + ":"
+                + "check-in: " + CheckIn.ToString("dd/MM/yyyy")
+                + ", check-out: " 
+                + CheckOut.ToString("dd/MM/yyyy") + ", " + Duration() + " noites";
+
+        }
+    }
+
+}
--- TratamentoDeExcecoes1/Program.cs
+++ TratamentoDeExcecoes1/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using TratamentoDeExcecoes1.Entities;
 
 namespace TratamentoDeExcecoes1
 {
@@ -6,7 +7,52 @@ namespace TratamentoDeExcecoes1
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.Write("Número do quarto: ");
+            int number = int.Parse(Console.ReadLine());
+            Console.Write("Check-in date (dd/MM/yyyy): ");
+            DateTime checkIn = DateTime.Parse(Console.ReadLine());
+            Console.Write("Check-out date (dd/MM/yyyy): ");
+            DateTime checkOut = DateTime.Parse(Console.ReadLine());
+
+            Console.WriteLine();
+
+            if (checkOut <= checkIn)
+            {
+                Console.WriteLine("Erro na reserva: O lançamento do checkOut não pode ser anterior ou igual ao checkIn!" );
+            }
+
+            else
+            {
+                Reservas reserva = new Reservas(number, checkIn, checkOut);
+                Console.WriteLine("Reserva: " + reserva );
+                Console.WriteLine();
+                Console.WriteLine("Informe os dados para atualização da reserva: ");
+                Console.Write("Check-in date (dd/MM/yyyy): ");
+                checkIn = DateTime.Parse(Console.ReadLine());
+                Console.Write("Check-out date (dd/MM/yyyy): ");
+                checkOut = DateTime.Parse(Console.ReadLine());
+
+                DateTime hoje = DateTime.Now;
+                if (checkIn < hoje || checkOut < hoje)
+                {
+                    Console.WriteLine("Erro na reserva: o check-In e o check-Out devem ser datas futuras!");
+                }
+                else if (checkOut <= checkIn)
+                {
+                    Console.WriteLine("Erro na reserva: O lançamento do checkOut não pode ser anterior ou igual ao checkIn!");
+                }
+
+                else
+                {
+                    reserva.UpdadeDates(checkIn, checkOut);
+                    Console.WriteLine("Reserva: " + reserva);
+                }
+            }
+           
+
+
+
+
         }
     }
 }
